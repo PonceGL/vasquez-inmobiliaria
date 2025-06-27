c1843db952b6931c8703e79d5d9b57086d634eb0
“PonceGL” <poncianogl@hotmail.com>
2025-06-27 15:22:31 -0600

    feat(configuration): add commitlint configuration and husky commit message hook

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      'type-enum': [
+        2,
+        'always',
+        ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'test'],
+      ],
+      'scope-empty': [2, 'never'],
+    },
+  };
+  
