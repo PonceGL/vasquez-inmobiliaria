2e544d6ef736b28c5c37c147ee897f22e1e3beae
“PonceGL” <poncianogl@hotmail.com>
2025-09-18 09:26:55 -0600

    refactor(project): remove old files and update next js

--- styles/denied/style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Main = styled.main`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
