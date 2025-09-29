09bb42348087be43729fa4411249bf46251a5e4c
“PonceGL” <poncianogl@hotmail.com>
2025-09-29 12:08:56 -0600

    fix(auth): improve error handling in isAuthenticated function

--- src/lib/auth/index.ts
+++ src/lib/auth/index.ts
@@ -1,20 +1,30 @@
 import { JWTPayload, jwtVerify } from "jose";
+import { JWTExpired } from "jose/errors";
 import { NextRequest } from "next/server";
 
 import { env } from "@/config/env";
+import { IS_DEV } from "@/constants/enviroment";
 import { AuthenticationError } from "@/lib/httpErrors";
 
 export async function isAuthenticated(
   request: NextRequest
 ): Promise<{ payload: JWTPayload }> {
-  const authHeader = request.headers.get("authorization");
+  try {
+    const authHeader = request.headers.get("authorization");
 
-  const token = authHeader?.split(" ")[1];
-  if (!token) {
-    throw new AuthenticationError("Authorization token is missing");
+    const token = authHeader?.split(" ")[1];
+    if (!token) {
+      throw new AuthenticationError("Authorization token is missing");
+    }
+    const secret = new TextEncoder().encode(env.SESSION_SECRET);
+    const { payload } = await jwtVerify(token, secret);
+
+    return { payload };
+  } catch (error) {
+    throw new AuthenticationError(
+      `Invalid authorization token${
+        IS_DEV ? `: ${(error as JWTExpired)?.code}` : "."
+      }`
+    );
   }
-  const secret = new TextEncoder().encode(env.SESSION_SECRET);
-  const { payload } = await jwtVerify(token, secret);
-
-  return { payload };
 }
