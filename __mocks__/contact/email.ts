ec4918ac5b866dd03a8d1c2ab0d9b17059b255aa
“PonceGL” <poncianogl@hotmail.com>
2025-10-06 14:17:12 -0600

    feat(email service): add comprehensive tests including CRUD operations and error handling

---
+++ __mocks__/contact/email.ts
@@ -0,0 +1,46 @@
+export const mockEmail = {
+  _id: "68e4223cd544a0b016703e8b",
+  label: "Otro",
+  email: "test@mail.com.mx",
+  isMain: false,
+  createdAt: "2025-10-06T20:10:36.915Z",
+  updatedAt: "2025-10-06T20:10:36.915Z",
+  __v: 0,
+};
+
+export const mockCreateEmail = {
+  label: "Otro",
+  email: "test@mail.com.mx",
+  isMain: false,
+};
+
+export const mockCreateEmailDto = {
+  label: "Otro",
+  email: "test@mail.com.mx",
+  isMain: false,
+};
+
+export const mockUpdateEmailDto = {
+  label: "Ventas",
+  email: "ventas@empresa.com.mx",
+  isMain: true,
+};
+
+export const mockEmailList = [
+  {
+    _id: "68e31dba6d1c3e1165d62316",
+    label: "Ventas",
+    email: "civsa@grupovasquez.com.mx",
+    isMain: true,
+    createdAt: "2025-10-06T01:39:06.139Z",
+    updatedAt: "2025-10-06T01:44:15.405Z",
+  },
+  {
+    _id: "68e31dd96d1c3e1165d6231b",
+    label: "Contabilidad",
+    email: "contaduria@grupovasquez.com.mx",
+    isMain: false,
+    createdAt: "2025-10-06T01:39:37.098Z",
+    updatedAt: "2025-10-06T01:44:15.385Z",
+  },
+];
