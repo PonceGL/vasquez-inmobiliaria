9a32a67b474771eb90777a4a49a0e436273069a1
“PonceGL” <poncianogl@hotmail.com>
2025-10-06 14:28:45 -0600

    feat(contact number service): add comprehensive tests for CRUD operations and error handling

---
+++ __mocks__/contact/number.ts
@@ -0,0 +1,78 @@
+export const mockContactNumber = {
+  _id: "68e424b2d544a0b016703e9c",
+  label: "Ventas",
+  phone: "1234567890",
+  hasWhatsApp: true,
+  isMainWhatsApp: true,
+  isMainCall: true,
+  createdAt: "2025-10-06T20:21:06.054Z",
+  updatedAt: "2025-10-06T20:21:06.054Z",
+  __v: 0,
+};
+
+export const mockCreateContactNumber = {
+  label: "Ventas",
+  phone: "1234567890",
+  hasWhatsApp: true,
+  isMainWhatsApp: true,
+  isMainCall: true,
+};
+
+export const mockCreateContactNumberDto = {
+  label: "Ventas",
+  phone: "1234567890",
+  hasWhatsApp: true,
+  isMainWhatsApp: true,
+  isMainCall: true,
+};
+
+export const mockUpdateContactNumberDto = {
+  label: "Ventas",
+  phone: "9876543210",
+  hasWhatsApp: false,
+  isMainWhatsApp: false,
+  isMainCall: false,
+};
+
+export const mockContactNumberList = [
+  {
+    _id: "68e301f094e402caabeaf16c",
+    label: "Ventas",
+    phone: "2282107188",
+    hasWhatsApp: true,
+    isMainWhatsApp: false,
+    isMainCall: false,
+    createdAt: "2025-10-05T23:40:32.848Z",
+    updatedAt: "2025-10-06T01:18:09.316Z",
+  },
+  {
+    _id: "68e3165e6d1c3e1165d622f2",
+    label: "Ventas",
+    phone: "2288372786",
+    hasWhatsApp: true,
+    isMainWhatsApp: false,
+    isMainCall: false,
+    createdAt: "2025-10-06T01:07:42.522Z",
+    updatedAt: "2025-10-06T01:08:01.632Z",
+  },
+  {
+    _id: "68e318786d1c3e1165d622fb",
+    label: "Ventas",
+    phone: "2288262433",
+    hasWhatsApp: false,
+    isMainWhatsApp: false,
+    isMainCall: false,
+    createdAt: "2025-10-06T01:16:40.686Z",
+    updatedAt: "2025-10-06T01:18:09.321Z",
+  },
+  {
+    _id: "68e318d16d1c3e1165d622ff",
+    label: "Ventas",
+    phone: "2281550849",
+    hasWhatsApp: true,
+    isMainWhatsApp: false,
+    isMainCall: false,
+    createdAt: "2025-10-06T01:18:09.323Z",
+    updatedAt: "2025-10-06T01:18:45.136Z",
+  },
+];
